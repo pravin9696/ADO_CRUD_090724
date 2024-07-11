cdd85a2cc503ae3fc7a8cd4585f1d25b9f13caf0
pravin gaikwad <pravingaikwad.sibar@outlook.com>
2024-07-11 09:59:00 +0530

    select , search using sqlDataReader added

--- Program.cs
+++ Program.cs
@@ -88,12 +88,68 @@ namespace ADO_CRUD_090724
 
             #region  delete logic ADO.net
 
-            int rl;
-           
+            //int rl;
 
-            Console.WriteLine("Enter roll to delete record from database");
-            rl = int.Parse(Console.ReadLine());
-           
+
+            //Console.WriteLine("Enter roll to delete record from database");
+            //rl = int.Parse(Console.ReadLine());
+
+            ////step1
+            //SqlConnection con = new SqlConnection("Data Source=DESKTOP-IRFMLF6\\SQLEXPRESS;Initial Catalog=dbTemp;Integrated Security=True;TrustServerCertificate=True");
+            //con.Open();
+
+            ////step2
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.Connection = con;
+            ////cmd.CommandText = "insert into tblStudent090724 values(" + rl + ",'" + nm + "'," + contact + ",'" + add1 + "')";
+            //cmd.CommandText = $"delete  from tblStudent090724 where roll={rl}";
+            //cmd.CommandType = CommandType.Text;
+
+            ////step3
+
+            //int n = cmd.ExecuteNonQuery();
+            //if (n > 0)
+            //{
+            //    Console.WriteLine("record Deleted  successully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("record Not found to Delete!!!");
+            //}
+
+            //con.Close();
+            #endregion
+
+            #region Select demo using  ADO.net
+
+
+            // //step1
+            // SqlConnection con = new SqlConnection("Data Source=DESKTOP-IRFMLF6\\SQLEXPRESS;Initial Catalog=dbTemp;Integrated Security=True;TrustServerCertificate=True");
+            // con.Open();
+
+            // //step2
+            // SqlCommand cmd = new SqlCommand();
+            // cmd.Connection = con;
+            // //cmd.CommandText = "insert into tblStudent090724 values(" + rl + ",'" + nm + "'," + contact + ",'" + add1 + "')";
+            // cmd.CommandText = "select * from tblstudent090724";
+            // cmd.CommandType = CommandType.Text;
+
+            // //step3
+            //SqlDataReader rdr= cmd.ExecuteReader();
+
+            // while (rdr.Read())
+            // {               
+            //     Console.WriteLine("Roll :"+rdr[0]+" Name :" + rdr["name"]);
+
+            // }
+            // con.Close();
+            #endregion
+
+            #region check record availble or not using ADO.net
+
+            string nm;
+            Console.WriteLine("Enter Name to check student record is available or not");
+            nm=Console.ReadLine();
             //step1
             SqlConnection con = new SqlConnection("Data Source=DESKTOP-IRFMLF6\\SQLEXPRESS;Initial Catalog=dbTemp;Integrated Security=True;TrustServerCertificate=True");
             con.Open();
@@ -102,21 +158,20 @@ namespace ADO_CRUD_090724
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
             //cmd.CommandText = "insert into tblStudent090724 values(" + rl + ",'" + nm + "'," + contact + ",'" + add1 + "')";
-            cmd.CommandText = $"delete  from tblStudent090724 where roll={rl}";
+            cmd.CommandText = $"select * from tblstudent090724 where name='{nm}'";
             cmd.CommandType = CommandType.Text;
 
             //step3
-
-            int n = cmd.ExecuteNonQuery();
-            if (n > 0)
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
             {
-                Console.WriteLine("record Deleted  successully");
+                Console.WriteLine("student available....");
             }
             else
             {
-                Console.WriteLine("record Not found to Delete!!!");
+                Console.WriteLine("student not available!!!!!");
             }
-
+           
             con.Close();
             #endregion
 
